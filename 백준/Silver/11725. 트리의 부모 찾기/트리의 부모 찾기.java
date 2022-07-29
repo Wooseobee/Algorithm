abd80044347d7802929e116aa5d94df6caf221b1
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2022-07-29 15:07:48 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1456 ms, Memory: 89212 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+//DFS or BFS
+public class Main {
+    static boolean[] visit;
+    static int[] parent;
+    static ArrayList<Integer>[] list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        list = new ArrayList[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        visit = new boolean[N + 1];
+        parent = new int[N + 1];
+
+
+        for (int i = 0; i < N - 1; i++) {
+            String s[] = br.readLine().split(" ");
+
+            int x = Integer.parseInt(s[0]);
+            int y = Integer.parseInt(s[1]);
+
+            list[x].add(y);
+            list[y].add(x);
+        }
+        dfs(1);
+        for (int i = 2; i <= N; i++) {
+            System.out.println(parent[i]);
+        }
+    }
+
+    public static void dfs(int v) {
+        visit[v] = true;
+
+        for (int i : list[v]) {
+            if (!visit[i]) {
+                parent[i]=v;
+                dfs(i);
+            }
+        }
+    }
+}
