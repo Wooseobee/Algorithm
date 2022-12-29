bbe43a1d2c3c754ef467cf678359a741d422413b
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2022-12-29 21:35:39 +0900

    [level 1] Title: 폰켓몬, Time: 2.12 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,25 +1,17 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 
 class Solution {
-    public int solution(int[] nums) {
-        int answer = 0;
+    public static int solution(int[] nums) {
+        HashSet<Integer> hashSet = new HashSet<>();
 
-        Map<Integer, Integer> list = new HashMap<>();
-        
         for (int i = 0; i < nums.length; i++) {
-            if (!list.containsKey(nums[i])) {
-                list.put(nums[i], 1);
-            } else {
-                list.put(nums[i], list.get(nums[i]) + 1);
-            }
+            hashSet.add(nums[i]);
         }
 
-        answer = nums.length / 2;
-        if (list.size() < nums.length / 2) {
-            answer = list.size();
+        if (hashSet.size() > nums.length / 2) {
+            return nums.length / 2;
         }
 
-        return answer;
+        return hashSet.size();
     }
 }
