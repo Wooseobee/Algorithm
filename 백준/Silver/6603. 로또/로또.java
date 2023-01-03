cecccccd557e1cde998ade4ed6145f13df6bf898
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-01-03 21:48:59 +0900

    [Silver II] Title: 로또, Time: 172 ms, Memory: 16600 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,25 +1,27 @@
 import java.io.*;
-import java.util.*;
 
-public class Main {;
+public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] arr;
+    static int[] answer = new int[6];
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
         while (true) {
             String[] s = br.readLine().split(" ");
             int k = Integer.parseInt(s[0]);
 
-            if (k==0) break;
+            if (k == 0) break;
 
-            int[] answer = new int[6];
-            int[] arr = new int[k];
-            boolean[] visited = new boolean[k];
+            arr = new int[k];
+            visited = new boolean[k];
 
             for (int i = 0; i < k; i++) {
                 arr[i] = Integer.parseInt(s[i + 1]);
             }
-            bruteForce(arr);
+
+            dfs(0, 0);
             bw.write("\n");
         }
 
@@ -28,37 +30,24 @@ public class Main {;
         br.close();
     }
 
-    static void bruteForce(int[] arr) throws IOException {
-        Set<List<Integer>> set = new LinkedHashSet<>();
-        int n = arr.length;
-        for (int i = 0; i < n - 5; i++) {
-            for (int j = i+1; j < n - 4; j++) {
-                for (int k = j+1; k < n - 3; k++) {
-                    for (int l = k+1; l < n - 2; l++) {
-                        for (int m = l+1; m < n - 1; m++) {
-                            for (int o = m + 1; o < n; o++) {
-                                List<Integer> list = new ArrayList<>();
-                                list.add(arr[i]);
-                                list.add(arr[j]);
-                                list.add(arr[k]);
-                                list.add(arr[l]);
-                                list.add(arr[m]);
-                                list.add(arr[o]);
-                                set.add(list);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        
-        Iterator<List<Integer>> it = set.iterator();
-        while (it.hasNext()) {
-            List<Integer> list = it.next();
-            for (int v : list) {
-                bw.write(v + " ");
+    static void dfs(int depth, int start) throws IOException {
+        if (depth == 6) {
+            for (int i = 0; i < 6; i++) {
+                bw.write(answer[i] + " ");
             }
             bw.write("\n");
+            return;
+        }
+
+        for (int i = start; i < arr.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+
+                answer[depth] = arr[i];
+                dfs(depth + 1, i);
+
+                visited[i] = false;
+            }
         }
     }
 }
