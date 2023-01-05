3800a5b6de4600c45f80f96f1588121ea0aed26e
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-01-05 19:02:38 +0900

    [Silver I] Title: 최대공약수, Time: 636 ms, Memory: 106104 KB -BaekjoonHub

--- 백준/Silver/2824. 최대공약수/최대공약수.java
+++ 백준/Silver/2824. 최대공약수/최대공약수.java
@@ -1,50 +1,49 @@
-import java.util.*;
 import java.io.*;
-import java.math.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        BigInteger A = new BigInteger("1");
+        BigInteger B = new BigInteger("1");
 
         int n = Integer.parseInt(br.readLine());
-        BigInteger a = new BigInteger("1");
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for(int i =0 ;i < n ; i++)
-            a= a.multiply(new BigInteger(st.nextToken()));
+        for (int i = 0; i < n; i++) {
+            A = A.multiply(new BigInteger(st.nextToken()));
+        }
 
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        BigInteger b = new BigInteger("1");
+        for (int i = 0; i < m; i++) {
+            B = B.multiply(new BigInteger(st.nextToken()));
+        }
 
-        for(int i =0 ; i < m ; i++)
-            b= b.multiply(new BigInteger(st.nextToken()));
-
-        findGCD(a,b);
-    }
-
-    public static void findGCD(BigInteger a , BigInteger b) {
-        BigInteger big = a.compareTo(b) > 0 ? a : b;
-        BigInteger small = a.compareTo(b) > 0 ? b : a;
+        BigInteger big = A.compareTo(B) > 0 ? A : B;
+        BigInteger small = A.compareTo(B) > 0 ? B : A;
         BigInteger zero = new BigInteger("0");
-        while(true) {
+
+        while (true) {
             BigInteger mod = big.mod(small);
 
-            if(mod.compareTo(zero) == 0)
-                break;
+            if (mod.compareTo(zero) == 0) break;
 
             big = small;
             small = mod;
         }
+        sb.append(small.toString());
 
-        String ans = small.toString();
-
-        if(ans.length() > 9) {
-            ans = ans.substring(ans.length()-9);
+        if (sb.length() > 9) {
+            bw.write(sb.substring(sb.length() - 9));
+        } else {
+            bw.write(String.valueOf(sb));
         }
-
-        System.out.println(ans);
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
-// 출처 : https://velog.io/@jms8732/2824.-%EC%B5%9C%EB%8C%80%EA%B3%B5%EC%95%BD%EC%88%98
