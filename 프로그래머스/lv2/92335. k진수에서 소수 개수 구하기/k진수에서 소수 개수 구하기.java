a103fce843f9518c242e95cf1abe4f4692d1172e
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-01-09 15:35:35 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.07 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,44 +1,19 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 class Solution {
     public static int solution(int n, int k) {
         int answer = 0;
-        List<Integer> list = new ArrayList<>();
 
-        while (n > 0) {
-            list.add(n % k);
-            n /= k;
-        }
+        String[] s = Integer.toString(n, k).split("0");
 
-        Collections.reverse(list);
-
-        List<Integer> prime = new ArrayList<>();
-
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) != 0) {
-                prime.add(list.get(i));
-            } else {
-                if (!prime.isEmpty()) {
-                    if (checkPrime(prime)) answer++;
-                }
-                prime.clear();
+        for (String str : s) {
+            if (str.length() != 0 && checkPrime(str)) {
+                answer++;
             }
-        }
-
-        if (!prime.isEmpty()) {
-            if (checkPrime(prime)) answer++;
         }
 
         return answer;
     }
 
-    static boolean checkPrime(List<Integer> prime) {
-        String s = "";
-        for (int i = 0; i < prime.size(); i++) {
-            s = s.concat(String.valueOf(prime.get(i)));
-        }
+    static boolean checkPrime(String s) {
         long value = Long.parseLong(s);
         if (value == 1) return false;
         for (int i = 2; i <= Math.sqrt(value); i++) {
