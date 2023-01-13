57c229928b6c1b99adfe1bf72fd16cef7253c54d
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-01-13 11:20:08 +0900

    [Silver III] Title: N과 M (4), Time: 204 ms, Memory: 18884 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,38 @@
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n, m;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+
+        String[] input = br.readLine().split(" ");
+
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+
+        arr = new int[n];
+
+        dfs(0, 0);
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static void dfs(int idx, int start) throws IOException {
+        if (idx == m) {
+            for (int i = 0; i < m; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            arr[idx] = i + 1;
+            dfs(idx + 1, i);
+        }
+    }
+}
