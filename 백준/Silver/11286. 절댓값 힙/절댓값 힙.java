8fce6b318d1c9ddac782e59ac904ccff8911f2a0
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-01-16 00:55:21 +0900

    [Silver I] Title: 절댓값 힙, Time: 376 ms, Memory: 26092 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -1,157 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                } else {
+                    return o1[1] - o2[1];
+                }
+            }
+        });
 
-        ArrayList<Integer> heap = new ArrayList<>();
-        heap.add(0);
+        int n = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             int x = Integer.parseInt(br.readLine());
 
             if (x == 0) {
-                if (heap.size() == 1) {
-                    sb.append(0 + "\n");
+                if (pq.isEmpty()) {
+                    bw.write("0\n");
                 } else {
-                    sb.append(heap.remove(1) + "\n");
-                    if (heap.size() > 2) {
-                        heap.add(1, heap.get(heap.size()-1));
-                        heap.remove(heap.size() - 1);
-                    }
-                    int top = 1, left, right, tmp;
-                    while (true) {
-                        left = top * 2;
-                        right = top * 2 + 1;
-                        if (left >= heap.size()) {
-                            break;
-                        } else if (right >= heap.size()) {
-                            if (Math.abs(heap.get(top)) == Math.abs(heap.get(left))) {
-                                if (heap.get(top) > heap.get(left)) {
-                                    tmp = heap.get(left);
-                                    heap.set(left, heap.get(top));
-                                    heap.set(top, tmp);
-                                    top = left;
-                                } else {
-                                    break;
-                                }
-                            } else if (Math.abs(heap.get(top)) > Math.abs(heap.get(left))) {
-                                tmp = heap.get(left);
-                                heap.set(left, heap.get(top));
-                                heap.set(top, tmp);
-                                top = left;
-                            } else {
-                                break;
-                            }
-                        } else {
-                            if (Math.abs(heap.get(left)) > Math.abs(heap.get(right))) {
-                                if (Math.abs(heap.get(top)) > Math.abs(heap.get(right))) {
-                                    tmp = heap.get(right);
-                                    heap.set(right, heap.get(top));
-                                    heap.set(top, tmp);
-                                    top = right;
-                                } else if (Math.abs(heap.get(top)) == Math.abs(heap.get(right))) {
-                                    if (heap.get(top) > heap.get(right)) {
-                                        tmp = heap.get(right);
-                                        heap.set(right, heap.get(top));
-                                        heap.set(top, tmp);
-                                        top = right;
-                                    } else {
-                                        break;
-                                    }
-                                } else {
-                                    break;
-                                }
-                            } else if (Math.abs(heap.get(left)) < Math.abs(heap.get(right))) {
-                                if (Math.abs(heap.get(top)) > Math.abs(heap.get(left))) {
-                                    tmp = heap.get(left);
-                                    heap.set(left, heap.get(top));
-                                    heap.set(top, tmp);
-                                    top = left;
-                                } else if (Math.abs(heap.get(top)) == Math.abs(heap.get(left))) {
-                                    if (heap.get(top) > heap.get(left)) {
-                                        tmp = heap.get(left);
-                                        heap.set(left, heap.get(top));
-                                        heap.set(top, tmp);
-                                        top = left;
-                                    } else {
-                                        break;
-                                    }
-                                } else {
-                                    break;
-                                }
-                            } else {
-                                if (heap.get(left) > heap.get(right)) {
-                                    if (Math.abs(heap.get(top)) > Math.abs(heap.get(right))) {
-                                        tmp = heap.get(right);
-                                        heap.set(right, heap.get(top));
-                                        heap.set(top, tmp);
-                                        top = right;
-                                    } else if (Math.abs(heap.get(top)) == Math.abs(heap.get(right))) {
-                                        if (heap.get(top) > heap.get(right)) {
-                                            tmp = heap.get(right);
-                                            heap.set(right, heap.get(top));
-                                            heap.set(top, tmp);
-                                            top = right;
-                                        } else {
-                                            break;
-                                        }
-                                    } else {
-                                        break;
-                                    }
-                                } else {
-                                    if (Math.abs(heap.get(top)) > Math.abs(heap.get(left))) {
-                                        tmp = heap.get(left);
-                                        heap.set(left, heap.get(top));
-                                        heap.set(top, tmp);
-                                        top = left;
-                                    } else if (Math.abs(heap.get(top)) == Math.abs(heap.get(left))) {
-                                        if (heap.get(top) > heap.get(left)) {
-                                            tmp = heap.get(left);
-                                            heap.set(left, heap.get(top));
-                                            heap.set(top, tmp);
-                                            top = left;
-                                        } else {
-                                            break;
-                                        }
-                                    } else {
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
+                    bw.write(pq.poll()[0] + "\n");
                 }
             } else {
-                if (heap.size() == 1) {
-                    heap.add(x);
-                } else {
-                    heap.add(x);
-                    int index = heap.size() - 1;
-                    while (index > 1 && (Math.abs(heap.get(index)) <= Math.abs(heap.get(index / 2)))) {
-                        if (Math.abs(heap.get(index)) == Math.abs(heap.get(index / 2))) {
-                            if (heap.get(index) < heap.get(index / 2)) {
-                                heap.set(index, heap.get(index / 2));
-                                heap.set(index / 2, x);
-                            } else {
-                                break;
-                            }
-                        } else {
-                            heap.set(index, heap.get(index / 2));
-                            heap.set(index / 2, x);
-                        }
-                        index /= 2;
-                    }
-                }
+                pq.add(new int[]{x, Math.abs(x)});
             }
         }
+
+        bw.flush();
+        bw.close();
         br.close();
-        System.out.println(sb);
     }
 }
