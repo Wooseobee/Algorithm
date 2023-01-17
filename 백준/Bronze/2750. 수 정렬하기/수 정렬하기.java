e89e93a3c8248c091735481a9851169246e87d33
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-01-17 17:20:34 +0900

    [Bronze II] Title: 수 정렬하기, Time: 188 ms, Memory: 14988 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            pq.add(Integer.parseInt(br.readLine()));
+        }
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(pq.poll());
+        }
+        br.close();
+    }
+}
