ca7cab403701156ce6e8428476f595f9696bac82
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-01-20 12:11:28 +0900

    [Silver II] Title: 스타트와 링크, Time: 388 ms, Memory: 19096 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,14 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     static int n;
     static int[][] levels;
-    static int[][] teams;
-    static int[] visited;
-    static int[] team1, team2;
+    static boolean[] visited;
     static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
@@ -17,10 +14,7 @@ public class Main {
         n = Integer.parseInt(br.readLine());
 
         levels = new int[n][n];
-        teams = new int[n][n];
-        visited = new int[n];
-        team1 = new int[n/2];
-        team2 = new int[n/2];
+        visited = new boolean[n];
 
         for (int i = 0; i < n; i++) {
             String[] input = br.readLine().split(" ");
@@ -41,54 +35,28 @@ public class Main {
             return;
         }
         for (int k = i; k < n; k++) {
-            if (visited[k] == 0) {
-                visited[k] = 1;
-                team1[size] = k;
+            if (!visited[k]) {
+                visited[k] = true;
 
                 check(k,size + 1);
 
-                visited[k] = 0;
+                visited[k] = false;
             }
         }
     }
 
     static int checkPower() {
-        int score1 = 0, score2 = 0 , size = 0;
-        for (int i : team1) {
-            for (int j : team1) {
-                if (i != j) {
-                    teams[i][j] = 1;
-                }
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            int cnt = 0;
-            for (int j : team1) {
-                if (i == j) {
-                    cnt++;
-                }
-            }
-            if (cnt==0) team2[size++] = i;
-        }
-
-        for (int i : team2) {
-            for (int j : team2) {
-                if (i != j) {
-                    teams[i][j] = -1;
-                }
-            }
-        }
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (teams[i][j] == 1) {
+        int score1 = 0, score2 = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (visited[i]&& visited[j]) {
                     score1 += levels[i][j];
-                } else if (teams[i][j] == -1) {
+                    score1 += levels[j][i];
+                } else if (!visited[i] && !visited[j]) {
                     score2 += levels[i][j];
+                    score2 += levels[j][i];
                 }
             }
-        }
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(teams[i], 0);
         }
         return Math.abs(score1 - score2);
     }
