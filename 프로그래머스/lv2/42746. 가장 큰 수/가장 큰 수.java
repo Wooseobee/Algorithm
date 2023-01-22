bd954f5478cb1fe3a2a2cf9aecd83836e36029ca
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-01-22 18:00:47 +0900

    [level 2] Title: 가장 큰 수, Time: 1.82 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,27 +2,25 @@ import java.util.PriorityQueue;
 
 class Solution {
     public String solution(int[] numbers) {
-        String answer = "";
+        StringBuilder answer = new StringBuilder();
 
         PriorityQueue<String> pq = new PriorityQueue<>((o1, o2) -> {
             if (o1.length() == o2.length()) {
                 return Integer.parseInt(o2) - Integer.parseInt(o1);
             }
-            return Integer.parseInt(o2.concat(o1)) - Integer.parseInt(o1.concat(o2));
+            return Integer.parseInt(o2 + o1) - Integer.parseInt(o1 + o2);
         });
 
-        int zeroCnt = 0;
         for (int number : numbers) {
             pq.add(String.valueOf(number));
-            if (number == 0) zeroCnt++;
         }
 
         while (!pq.isEmpty()) {
-            answer = answer.concat(pq.poll());
+            answer.append(pq.poll());
         }
 
-        if (zeroCnt == numbers.length) return "0";
+        if (answer.charAt(0) == '0') return "0";
 
-        return answer;
+        return answer.toString();
     }
 }
