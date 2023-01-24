e489fe1b486a135541debe0ae1bac3257e75a460
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-01-25 00:11:36 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[n + 1][10];
+        long cnt = 9;
+
+        for (int i = 1; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            cnt = 0;
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][1] % 1_000_000_000;
+                } else if (j == 9) {
+                    dp[i][j] = dp[i - 1][8] % 1_000_000_000;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1_000_000_000;
+                }
+                if (i == n) cnt += (dp[i][j] % 1_000_000_000);
+            }
+        }
+        System.out.println(cnt % 1_000_000_000);
+        br.close();
+    }
+}
