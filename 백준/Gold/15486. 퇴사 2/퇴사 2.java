5ee35d175db20ebcf2745d397fb1844b9aa56c1c
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-01-29 16:50:09 +0900

    [Gold V] Title: 퇴사 2, Time: 844 ms, Memory: 317116 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,34 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int max = Integer.MIN_VALUE;
 
-        int[] t = new int[n + 2];
-        int[] p = new int[n + 2];
         int[] dp = new int[n + 2];
+        int[] t = new int[n + 1];
+        int[] p = new int[n + 1];
+
+        String[] input;
+        for (int i = 0; i < n; i++) {
+            input = br.readLine().split(" ");
+            t[i + 1] = Integer.parseInt(input[0]);
+            p[i + 1] = Integer.parseInt(input[1]);
+        }
 
         for (int i = 1; i <= n; i++) {
-            String[] s = br.readLine().split(" ");
-
-            t[i] = Integer.parseInt(s[0]);
-            p[i] = Integer.parseInt(s[1]);
-        }
-
-        for (int i = 1; i <= n + 1; i++) {
-            max = Math.max(max, dp[i]);
-
-            int day = i + t[i];
-            if (day <= n + 1) {
-                dp[day] = Math.max(max + p[i], dp[day]);
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+            if (i + t[i] <= n + 1) {
+                dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]);
             }
         }
-        System.out.println(max);
+        System.out.println(Math.max(dp[n], dp[n + 1]));
         br.close();
     }
 }
