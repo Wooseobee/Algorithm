2e12057a6d341af147a10a52bd6945b16c87b2c4
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-02-03 15:32:30 +0900

    [Gold V] Title: 동전 2, Time: 132 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -2,70 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, k, min = Integer.MAX_VALUE;
-    static int[] coins, values;
-    static boolean[] visited;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = br.readLine().split(" ");
 
-        n = Integer.parseInt(input[0]);
-        k = Integer.parseInt(input[1]);
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
 
-        coins = new int[n];
-        values = new int[k + 1];
-        visited = new boolean[k + 1];
+        int[] coins = new int[n];
+        int[] dp = new int[k + 1];
 
         for (int i = 0; i < n; i++) {
             coins[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.fill(values, 10_001);
-
-        for (int j = 0; j < n; j++) {
-            if (coins[j] <= k) bfs(coins[j]);
-            Arrays.fill(visited, false);
-        }
-
-        if (values[k] == 10_001) {
-            System.out.println(-1);
-        } else {
-            System.out.println(values[k]);
-        }
-        br.close();
-    }
-
-    static class Coin {
-        int value;
-        int count;
-
-        public Coin(int value, int count) {
-            this.value = value;
-            this.count = count;
-        }
-    }
-
-    static void bfs(int value) {
-        Queue<Coin> q = new LinkedList<>();
-        q.add(new Coin(value, 1));
-        visited[value] = true;
-
-        while (!q.isEmpty()) {
-            Coin coin = q.poll();
-            int v = coin.value;
-            int c = coin.count;
-
-            values[v] = Math.min(values[v], c);
-            if (v == k) return;
-
-            for (int i = 0; i < n; i++) {
-                if (v + coins[i] <= k && !visited[v + coins[i]]) {
-                    q.add(new Coin(v + coins[i], c + 1));
-                    visited[v + coins[i]] = true;
-                }
+        Arrays.fill(dp, 10_001);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = coins[i]; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
             }
         }
+
+        if (dp[k] == 10_001) {
+            System.out.println(-1);
+        } else {
+            System.out.println(dp[k]);
+        }
+        br.close();
     }
 }
