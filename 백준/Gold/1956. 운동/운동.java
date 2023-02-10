bf8c2afd588f7222beb9985e5d77f6e0c7ad7b60
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-02-10 18:20:42 +0900

    [Gold IV] Title: 운동, Time: 712 ms, Memory: 71716 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int[][] dist;
+    static final int INF = 4_000_001;
+    static int min = INF;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+
+        int v = Integer.parseInt(input[0]);
+        int e = Integer.parseInt(input[1]);
+
+        dist = new int[v + 1][v + 1];
+
+        for (int i = 1; i <= v; i++) {
+            Arrays.fill(dist[i], INF);
+        }
+
+        for (int j = 0; j < e; j++) {
+            input = br.readLine().split(" ");
+            dist[Integer.parseInt(input[0])][Integer.parseInt(input[1])] = Integer.parseInt(input[2]);
+        }
+
+        FloydWarshall(v);
+
+        for (int i = 1; i <= v; i++) {
+            min = Math.min(min, dist[i][i]);
+        }
+
+        if (min == INF) {
+            System.out.println(-1);
+        } else {
+            System.out.println(min);
+        }
+
+        br.close();
+    }
+
+    static void FloydWarshall(int v) {
+        for (int k = 1; k <= v; k++) {
+            for (int i = 1; i <= v; i++) {
+                for (int j = 1; j <= v; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+}
