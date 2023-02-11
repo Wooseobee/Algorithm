86457bb6d2e6421775418faa980e4d8ce0cb22a5
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-02-11 23:28:12 +0900

    [level 2] Title: 조이스틱, Time: 0.36 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.java
@@ -1,21 +1,53 @@
+import java.util.Arrays;
+
 class Solution {
-    public int solution(String name) {
+    public static int solution(String name) {
         int answer = 0;
         int len = name.length();
-
-        int move = len - 1;
+        char[] answerName = new char[len];
         for (int i = 0; i < len; i++) {
-            answer += Math.min(Math.abs(name.charAt(i) - 'A'), Math.abs(name.charAt(i) - 'Z') + 1);
-
-            int endA = i + 1;
-            while (endA < len && name.charAt(endA) == 'A') {
-                endA++;
-            }
-            move = Math.min(move, i * 2 + (len - endA));
-            move = Math.min(move, i + (len - endA) * 2);
+            answerName[i] = 'A';
         }
 
-        System.out.println(answer + move);
-        return answer + move;
+        int idx = 0;
+        while (true) {
+            if (answerName[idx] != name.charAt(idx)) {
+                answer += Math.min(Math.abs(name.charAt(idx) - 'A'), Math.abs(name.charAt(idx) - 'Z') + 1);
+                answerName[idx] = name.charAt(idx);
+            }
+
+            if (checkFinish(answerName, name)) break;
+
+            int leftCnt = 0, rightCnt = 0;
+            int left = idx, right = idx;
+            while (answerName[left] == name.charAt(left)) {
+                left--;
+                leftCnt++;
+                if (left == -1) left = len - 1;
+            }
+            while (answerName[right] == name.charAt(right)) {
+                right++;
+                rightCnt++;
+                if (right == len) right = 0;
+            }
+            if (leftCnt >= rightCnt) {
+                answer += rightCnt;
+                idx = right;
+            } else {
+                answer += leftCnt;
+                idx = left;
+            }
+        }
+
+        System.out.println(answer);
+        return answer;
+    }
+
+    static boolean checkFinish(char[] answerName, String name) {
+        return String.valueOf(answerName).equals(name);
+    }
+
+    public static void main(String[] args) {
+        solution("JAZ");
     }
 }
