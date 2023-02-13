1cbd23aaf3f722c23f63b8f3e3a6310720914f64
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-02-13 17:45:59 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.54 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -2,72 +2,23 @@ import java.util.*;
 
 class Solution {
     public String solution(String new_id) {
-        new_id = new_id.toLowerCase();
-        new_id = chap2(new_id);
-        new_id = chap3(new_id);
-        if (new_id.length() > 0) {
-            new_id = new_id.charAt(0) == '.' ? new_id.replaceFirst(".", "") : new_id;
-        }
-        if (new_id.length() > 0 && new_id.charAt(new_id.length() - 1) == '.') {
-            new_id = new_id.substring(0, new_id.length() - 1);
-        }
-        if (new_id.contains(" ")) {
-            new_id = new_id.replace(" ", "a");
-        }
-        if (new_id.equals("")) {
-            new_id = "a";
-        }
+        String answer = new_id.toLowerCase();
 
-        if (new_id.length() > 15) {
-            new_id = new_id.substring(0, 15);
-            if (new_id.charAt(new_id.length() - 1) == '.') {
-                new_id = new_id.substring(0, new_id.length() - 1);
-            }
-        } else if (new_id.length() <= 2) {
-            while (new_id.length() < 3) {
-                new_id = new_id.concat(String.valueOf(new_id.charAt(new_id.length() - 1)));
+        answer = answer.replaceAll("[^-_.a-z0-9]", "");
+        answer = answer.replaceAll("[.]{2,}", ".");
+        answer = answer.replaceAll("^[.]|[.]$", "");
+        if (answer.equals("")) {
+            answer += "a";
+        }
+        if (answer.length() > 15) {
+            answer = answer.substring(0, 15);
+            answer = answer.replaceAll("[.]$", "");
+        }
+        if (answer.length() <= 2) {
+            while (answer.length() < 3) {
+                answer += answer.charAt(answer.length() - 1);
             }
         }
-
-        return new_id;
-    }
-
-    static String chap2(String id) {
-        String new_id = id;
-        Set<String> list = new HashSet<>();
-        for (int i = 0; i < 26; i++) {
-            list.add(Character.toString('a' + i));
-        }
-        for (int i = 0; i < 10; i++) {
-            list.add(String.valueOf(i));
-        }
-        list.add("-");
-        list.add("_");
-        list.add(".");
-
-        for (int i = 0; i < new_id.length(); i++) {
-            char ch = new_id.charAt(i);
-            if (!list.contains(String.valueOf(ch))) {
-                new_id = new_id.replace(String.valueOf(ch), "");
-                i--;
-            }
-        }
-
-        return new_id;
-    }
-
-    static String chap3(String id) {
-        String new_id = id;
-
-        for (int i = 0; i < new_id.length(); i++) {
-            if (new_id.charAt(i) == '.') {
-                int j = i + 1;
-                while (j < new_id.length() && new_id.charAt(j) == '.') {
-                    j++;
-                }
-                new_id = new_id.substring(0, i) + new_id.substring(j - 1);
-            }
-        }
-        return new_id;
+        return answer;
     }
 }
