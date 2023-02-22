ba20137d32b52c735711cca0df9aa1bbbdc96aa9
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-02-22 14:40:29 +0900

    [Silver II] Title: 색종이 만들기, Time: 196 ms, Memory: 17248 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -1,18 +1,15 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-    static int n;
     static int[][] paper;
     static int white = 0, blue = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
-
+        int n = Integer.parseInt(br.readLine());
         paper = new int[n][n];
+
 
         for (int i = 0; i < n; i++) {
             String[] input = br.readLine().split(" ");
@@ -21,31 +18,29 @@ public class Main {
             }
         }
 
-        countColor(0, 0, n);
+        count(0, 0, n);
 
         System.out.println(white + "\n" + blue);
+        br.close();
     }
 
-    static void countColor(int i, int j, int size) {
-        if (checkColor(i, j, size)) {
-            if (paper[i][j]==0) white++;
-            else blue++;
+    static void count(int i, int j, int size) {
+        if (sameColor(i, j, size)) {
+            if (paper[i][j] == 1) blue++;
+            else white++;
             return;
         }
-
-        countColor(i, j, size / 2);
-        countColor(i, j + size / 2, size / 2);
-        countColor(i + size / 2, j, size / 2);
-        countColor(i + size / 2, j + size / 2, size / 2);
+        count(i, j, size / 2);
+        count(i, j + size / 2, size / 2);
+        count(i + size / 2, j, size / 2);
+        count(i + size / 2, j + size / 2, size / 2);
     }
 
-    static boolean checkColor(int i, int j, int size) {
+    static boolean sameColor(int i, int j, int size) {
         int color = paper[i][j];
         for (int k = i; k < i + size; k++) {
             for (int l = j; l < j + size; l++) {
-                if (paper[k][l] != color) {
-                    return false;
-                }
+                if (paper[k][l] != color) return false;
             }
         }
         return true;
