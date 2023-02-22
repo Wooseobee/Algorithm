838b6411ab44428ce7b7e39a88c3b8e8750aa6e7
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-02-22 11:04:08 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 612 ms, Memory: 33184 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.java
@@ -1,16 +1,29 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static final int max = 1_000_001;
+    static boolean[] prime = new boolean[max + 1];
 
     public static void main(String[] args) throws IOException {
+
+        Arrays.fill(prime, true);
+        prime[0] = prime[1] = false;
+        for (int i = 2; i * i <= max; i++) {
+            if (prime[i]) {
+                for (int j = i * i; j <= max; j += i) {
+                    prime[j] = false;
+                }
+            }
+        }
 
         while (true) {
             int n = Integer.parseInt(br.readLine());
 
             if (n == 0) break;
-            Goldbach(n / 2);
+            Goldbach(n);
         }
 
         bw.flush();
@@ -19,10 +32,10 @@ public class Main {
     }
 
     static void Goldbach(int n) throws IOException {
-        int left = 3, right = (n * 2) - 3;
-        while (left <= n) {
-            if (isDecimal(left) && isDecimal(right)) {
-                bw.write(n * 2 + " = " + left + " + " + right + "\n");
+        int left = 3, right = n - 3;
+        while (left <= right) {
+            if (prime[left] && prime[right]) {
+                bw.write(n + " = " + left + " + " + right + "\n");
                 return;
             } else {
                 left += 2;
@@ -30,12 +43,5 @@ public class Main {
             }
         }
         bw.write("Goldbach's conjecture is wrong.\n");
-    }
-
-    static boolean isDecimal(int value) {
-        for (int i = 2; i <= Math.sqrt(value); i++) {
-            if (value % i == 0) return false;
-        }
-        return true;
     }
 }
