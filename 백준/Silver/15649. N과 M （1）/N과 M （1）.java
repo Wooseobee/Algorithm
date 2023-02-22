9b7ef6512d48c5d1bf9b5eaaecc1435729d6ca3f
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-02-22 10:33:38 +0900

    [Silver III] Title: N과 M (1), Time: 280 ms, Memory: 22308 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,41 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int n, m;
-    static int[] arr;
     static boolean[] visited;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] s = br.readLine().split(" ");
-
-        n = Integer.parseInt(s[0]);
-        m = Integer.parseInt(s[1]);
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
 
         arr = new int[m];
         visited = new boolean[n];
 
-        dfs(0);
-        
+        backTracking(0, n, m);
+        System.out.println(sb);
         br.close();
     }
 
-    public static void dfs(int depth) {
-        if (depth == m) {
-            for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + " ");
+    public static void backTracking(int idx, int n, int m) {
+        if (idx == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(arr[i]).append(" ");
             }
-            System.out.println();
+            sb.append("\n");
             return;
         }
 
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-
                 visited[i] = true;
 
-                arr[depth] = i + 1;
-                dfs(depth + 1);
+                arr[idx] = i + 1;
+                backTracking(idx + 1, n, m);
 
                 visited[i] = false;
             }
