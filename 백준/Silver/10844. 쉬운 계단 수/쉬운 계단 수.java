fe47a74748eea80c4fd6f7a1a643c0165a26af81
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-02-22 18:32:09 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 120 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -1,33 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
+    static final int mod = 1_000_000_000;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
         int[][] dp = new int[n + 1][10];
-        long cnt = 9;
 
         for (int i = 1; i < 10; i++) {
             dp[1][i] = 1;
         }
 
         for (int i = 2; i <= n; i++) {
-            cnt = 0;
             for (int j = 0; j < 10; j++) {
                 if (j == 0) {
-                    dp[i][j] = dp[i - 1][1] % 1_000_000_000;
+                    dp[i][j] = dp[i - 1][1] % mod;
                 } else if (j == 9) {
-                    dp[i][j] = dp[i - 1][8] % 1_000_000_000;
+                    dp[i][j] = dp[i - 1][8] % mod;
                 } else {
-                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % 1_000_000_000;
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % mod;
                 }
-                if (i == n) cnt += (dp[i][j] % 1_000_000_000);
             }
         }
-        System.out.println(cnt % 1_000_000_000);
+
+        long result = 0;
+        for (int i = 0; i < 10; i++) {
+            result += dp[n][i];
+        }
+
+        System.out.println(result % mod);
         br.close();
     }
 }
