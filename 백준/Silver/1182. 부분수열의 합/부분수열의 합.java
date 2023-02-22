e5d86c01e949e72326bde46724127954671ae2ff
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-02-22 12:37:42 +0900

    [Silver II] Title: 부분수열의 합, Time: 132 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -1,40 +1,36 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     static int[] arr;
-    static int n, s;
-    static int cnt = 0;
+    static int cnt = 0, n, s;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] input = br.readLine().split(" ");
-
         n = Integer.parseInt(input[0]);
         s = Integer.parseInt(input[1]);
 
         arr = new int[n];
 
         input = br.readLine().split(" ");
-
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(input[i]);
         }
 
-        dfs(0, 0);
+        solution(0, 0);
         System.out.println(s == 0 ? cnt - 1 : cnt);
         br.close();
     }
 
-    public static void dfs(int depth, int sum) {
+    static void solution(int depth, int sum) {
         if (depth == n) {
             if (sum == s) cnt++;
             return;
         }
 
-        dfs(depth + 1, sum + arr[depth]);
-        dfs(depth + 1, sum);
+        solution(depth + 1, sum + arr[depth]);
+        solution(depth + 1, sum);
     }
 }
