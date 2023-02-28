eec4125dd28e5ae500eb212b237e3a9d769b657b
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-02-28 15:30:13 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 124 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -3,8 +3,7 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static int[] s,b, choice;
-    static boolean[] visited;
+    static int[] s, b;
     static long min = Long.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
@@ -13,7 +12,6 @@ public class Main {
         n = Integer.parseInt(br.readLine());
         s = new int[n];
         b = new int[n];
-        visited = new boolean[n];
 
         for (int i = 0; i < n; i++) {
             String[] str = br.readLine().split(" ");
@@ -21,40 +19,18 @@ public class Main {
             b[i] = Integer.parseInt(str[1]);
         }
 
-        for (int i = 1; i <= n; i++) {
-            choice = new int[i];
-            dfs(0, 0, i);
-        }
+        dfs(0, 0, 1, 0);
 
         System.out.println(min);
         br.close();
     }
 
-    static void dfs(int start, int depth, int size) {
-        if (depth == size) {
-            findMin(size);
+    static void dfs(int idx, int depth, int totalS, int totalB) {
+        if (idx == n) {
+            if (depth != 0) min = Math.min(min, Math.abs(totalS - totalB));
             return;
         }
-        for (int i = start; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-
-                choice[depth] = i;
-                dfs(i, depth + 1, size);
-
-                visited[i] = false;
-            }
-        }
-    }
-
-    private static void findMin(int size) {
-        long totalS = 1;
-        long totalB = 0;
-        for (int i = 0; i < size; i++) {
-            int idx = choice[i];
-            totalS *= s[idx];
-            totalB += b[idx];
-        }
-        min = Math.min(min, Math.abs(totalS - totalB));
+        dfs(idx + 1, depth, totalS, totalB);
+        dfs(idx + 1, depth + 1, totalS * s[idx], totalB + b[idx]);
     }
 }
