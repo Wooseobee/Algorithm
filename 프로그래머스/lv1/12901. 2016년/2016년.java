a48f51fc1f16d3fd2e73f57a7afd2c36170738dc
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-03-04 01:04:16 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.java
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -1,30 +1,12 @@
 class Solution {
-    static final String[] day = new String[]{"SUN","MON","TUE","WED","THU","FRI","SAT"};
-
     public String solution(int a, int b) {
-        String[][] date = new String[13][32];
-        int idx = 5;
-
-        for(int i=1;i<=12;i++){
-            int limit = 0;
-            switch (i){
-                case 1:case 3:case 5:case 7:case 8:case 10:case 12:
-                    limit=31;
-                    break;
-                case 2:
-                    limit=29;
-                    break;
-                case 4:case 6:case 9:case 11:
-                    limit=30;
-                    break;
-            }
-            for(int j=1;j<=limit;j++){
-                if(idx>6){
-                    idx = 0;
-                }
-                date[i][j] = day[idx++];
-            }
+        String[] day = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
+        int[] date = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int allDate = 0;
+        for (int i = 0; i < a - 1; i++) {
+            allDate += date[i];
         }
-        return date[a][b];
+        allDate += b;
+        return day[allDate % 7];
     }
 }
