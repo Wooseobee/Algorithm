e4d607a49328735d827c778ca6416afa027e6954
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-03-03 21:59:51 +0900

    [level 3] Title: 여행경로, Time: 8.83 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/lv3/43164. 여행경로/여행경로.java
+++ 프로그래머스/lv3/43164. 여행경로/여행경로.java
@@ -1,82 +1,38 @@
 import java.util.*;
 
 class Solution {
-    static Map<String, List<Ticket>> map = new HashMap<>();
-    static List<String> ansList = new ArrayList<>();
     static int ticketTotal;
+    static boolean[] visited;
+    static String course;
 
     public String[] solution(String[][] tickets) {
         ticketTotal = tickets.length;
+        visited = new boolean[ticketTotal];
 
-        for (int i = 0; i < tickets.length; i++) {
-            String start = tickets[i][0];
-            String end = tickets[i][1];
-            if (!map.containsKey(start)) {
-                map.put(start, new ArrayList<>());
-            }
-            map.get(start).add(new Ticket(start, end, i));
-        }
-
-        for (List<Ticket> list : map.values()) {
-            list.sort((o1, o2) -> o1.end.compareTo(o2.end));
-        }
-
-        bfs();
-
-        return ansList.toArray(new String[0]);
-    }
-
-    static class Ticket {
-        String start;
-        String end;
-        int ticketNum;
-
-        public Ticket(String start, String end, int ticketNum) {
-            this.start = start;
-            this.end = end;
-            this.ticketNum = ticketNum;
-        }
-
-        @Override
-        public boolean equals(Object object) {
-            Ticket ticket = (Ticket) object;
-
-            return ticket.ticketNum == this.ticketNum;
-        }
-    }
-
-    static void bfs() {
-        Queue<List<Ticket>> q = new LinkedList<>();
-        for (Ticket t : map.get("ICN")) {
-            List<Ticket> list = new ArrayList<>();
-            list.add(new Ticket("ICN", t.end, t.ticketNum));
-            q.add(list);
-        }
-
-        while (!q.isEmpty()) {
-            List<Ticket> path = q.poll();
-            int size = path.size();
-            String end = path.get(size - 1).end;
-
-            if (size == ticketTotal) {
-                ansList.add("ICN");
-                for (Ticket t : path) {
-                    ansList.add(t.end);
+        Arrays.sort(tickets, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                if (o1[0].equals(o2[0])) {
+                    return o2[1].compareTo(o1[1]);
                 }
-                return;
+                return o1[0].compareTo(o2[0]);
             }
+        });
 
-            if (map.containsKey(end)) {
-                for (Ticket next : map.get(end)) {
-                    Ticket nextTicket = new Ticket(next.start, next.end, next.ticketNum);
-                    if (!path.contains(nextTicket)) {
-                        List<Ticket> newPath = new ArrayList<>();
-                        for (Ticket t : path) {
-                            newPath.add(t);
-                        }
-                        newPath.add(nextTicket);
-                        q.add(newPath);
-                    }
+        dfs(0, "ICN", "ICN", tickets);
+
+        return course.split(" ");
+    }
+
+    static void dfs(int depth, String start, String travelWay, String[][] tickets){
+        if (depth == ticketTotal) {
+            course = travelWay;
+        }else{
+            for (int i = 0; i < ticketTotal; i++) {
+                if (!visited[i] && tickets[i][0].equals(start)) {
+                    visited[i] = true;
+                    dfs(depth + 1, tickets[i][1], travelWay + " " + tickets[i][1], tickets);
+                    visited[i] = false;
                 }
             }
         }
