5573097d914e6426394af393c300b61d34b0da05
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-03-07 15:29:33 +0900

    [level 3] Title: 등굣길, Time: 0.57 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.java
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.java
@@ -2,31 +2,23 @@ class Solution {
     static int[][] map;
     
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
         map = new int[n+1][m+1];
         
         for(int i=0;i<puddles.length;i++){
             map[puddles[i][1]][puddles[i][0]] = -1;
         }
         
-        return dfs(1,1,n,m);
-    }
-    static int dfs(int i,int j, int n, int m){
-        if(i==n&&j==m){
-            return 1;
+        map[1][1] = 1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1&&j==1) continue;
+                if(map[i][j]==-1) {
+                    map[i][j]=0;
+                    continue;
+                }
+                map[i][j] = (map[i-1][j]+map[i][j-1]) % 1_000_000_007;
+            }
         }
-        
-        if(map[i][j]!=0) return map[i][j];
-        
-        if(i+1<=n && map[i+1][j]!=-1){
-            map[i][j] += dfs(i+1,j,n,m);
-            if(map[i][j]>1_000_000_007) map[i][j] %= 1_000_000_007;
-        }
-        if(j+1<=m &&map[i][j+1]!=-1){
-            map[i][j] += dfs(i,j+1,n,m);
-            if(map[i][j]>1_000_000_007) map[i][j] %= 1_000_000_007;
-        }
-        
-        return map[i][j];
+        return map[n][m];
     }
 }
