ea53ce0ce2bed7bed6ed9cce4234800b70457dda
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-03-08 17:52:41 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 1.38 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,22 +1,11 @@
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] strings = s.split("");
+        String[] strings = s.toLowerCase().split("");
         boolean changeWord = true;
-        for(int i=0;i<strings.length;i++){
-            String str = strings[i];
-            if(str.equals(" ")){
-                changeWord = true;
-            } else if(changeWord){
-                str = str.toUpperCase();
-                changeWord = false;
-            } else if(Character.isDigit(str.charAt(0))){
-                continue;
-            } else {
-                str = str.toLowerCase();
-            }
-            
-            answer+=str;
+        for(String str : strings){            
+            answer+=changeWord ? str.toUpperCase():str;
+            changeWord = str.equals(" ") ? true:false;
         }
         return answer;
     }
