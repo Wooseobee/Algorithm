06be76813175e4c5c4fa6226b880e97191703909
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-03-21 01:28:55 +0900

    [Gold V] Title: 배, Time: 200 ms, Memory: 16392 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -6,59 +6,46 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] crane = new int[n];
-        int maxCraneWeight = 0;
-        int minCraneWeight = Integer.MAX_VALUE;
+        Integer[] crane = new Integer[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < n; i++) {
             crane[i] = Integer.parseInt(st.nextToken());
-            maxCraneWeight = Math.max(maxCraneWeight, crane[i]);
-            minCraneWeight = Math.min(minCraneWeight, crane[i]);
         }
 
         int m = Integer.parseInt(br.readLine());
         List<Integer> box = new ArrayList<>();
-        int maxBoxWeight = 0;
         int minBoxWeight = Integer.MAX_VALUE;
 
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < m; i++) {
             box.add(Integer.parseInt(st.nextToken()));
-            maxBoxWeight = Math.max(maxBoxWeight, box.get(i));
             minBoxWeight = Math.min(minBoxWeight, box.get(i));
         }
 
-        crane = Arrays.stream(crane)
-                .boxed().sorted(Collections.reverseOrder())
-                .mapToInt(Integer::intValue).toArray();
-
+        Arrays.sort(crane, Collections.reverseOrder());
         box.sort(Collections.reverseOrder());
 
-        boolean canMove = maxBoxWeight <= maxCraneWeight;
+        if(crane[0]<box.get(0)){
+            System.out.println(-1);
+        } else {
+            int time = 0;
+            while (!box.isEmpty()) {
+                time++;
+                for (int i = 0; i < n; i++) {
+                    int w = crane[i];
+                    if (w < minBoxWeight) break;
 
-        int time = 0;
-        while (canMove) {
-            time++;
-            for (int i = 0; i < n; i++) {
-                int w = crane[i];
-                if (w < minBoxWeight) break;
-                
-                for (int j = 0; j < box.size(); j++) {
-                    if (w >= box.get(j)) {
-                        box.remove(j);
-                        break;
+                    int boxSize = box.size();
+                    for (int j = 0; j < boxSize; j++) {
+                        if (w >= box.get(j)) {
+                            box.remove(j);
+                            break;
+                        }
                     }
                 }
             }
-            if (box.isEmpty()) {
-                break;
-            }
-        }
-        if (canMove) {
             System.out.println(time);
-        } else {
-            System.out.println(-1);
         }
 
         br.close();
