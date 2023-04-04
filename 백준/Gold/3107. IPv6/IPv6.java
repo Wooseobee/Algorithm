4c926d132ec857ad9c8e5a49dac1623df3aaea2f
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-04-04 22:35:49 +0900

    [Gold V] Title: IPv6, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Gold/3107. IPv6/IPv6.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String ipv6 = br.readLine();
+
+        int colon = 0;
+        for (int i = 0; i < ipv6.length(); i++) {
+            if (ipv6.charAt(i) == ':') colon++;
+        }
+
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < ipv6.length(); i++) {
+            char ch = ipv6.charAt(i);
+            if (ch == ':') {
+                if ((i == 0 && ipv6.charAt(i + 1) == ':') ||
+                        (i == ipv6.length() - 2 && ipv6.charAt(i + 1) == ':')) {
+                    for (int j = colon; j <= 8; j++) {
+                        answer.append("0000:");
+                    }
+                } else if (i != ipv6.length() - 1 && ipv6.charAt(i + 1) == ':') {
+                    for (int j = colon; j < 8; j++) {
+                        answer.append("0000:");
+                    }
+                }
+            } else {
+                StringBuilder sb = new StringBuilder();
+                while (ch != ':') {
+                    i++;
+                    sb.append(ch);
+                    if (i == ipv6.length()) break;
+                    ch = ipv6.charAt(i);
+                }
+                sb.reverse();
+                while (sb.length() < 4) {
+                    sb.append('0');
+                }
+                answer.append(sb.reverse()).append(":");
+                i--;
+            }
+        }
+
+        System.out.println(answer.delete(answer.length() - 1, answer.length()));
+        br.close();
+    }
+}
