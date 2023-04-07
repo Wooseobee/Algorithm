d247e95bd52df0e9dc6f07f8b8e3e45170916e0c
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-04-07 18:19:23 +0900

    [Gold IV] Title: LCS 2, Time: 152 ms, Memory: 18252 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.java
+++ 백준/Gold/9252. LCS 2/LCS 2.java
@@ -2,31 +2,31 @@ import java.io.*;
 
 public class Main {
     static int lcs;
-    static boolean found = false;
     static char[] str1;
     static char[] str2;
     static int[][] dp;
-    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String s1 = br.readLine();
         String s2 = br.readLine();
+        int len1 = s1.length();
+        int len2 = s2.length();
 
-        str1 = new char[s1.length() + 1];
-        str2 = new char[s2.length() + 1];
-        dp = new int[str1.length][str2.length];
+        str1 = new char[len1 + 1];
+        str2 = new char[len2 + 1];
+        dp = new int[len1 + 1][len2 + 1];
 
-        for (int i = 0; i < s1.length(); i++) {
+        for (int i = 0; i < len1; i++) {
             str1[i + 1] = s1.charAt(i);
         }
-        for (int i = 0; i < s2.length(); i++) {
+        for (int i = 0; i < len2; i++) {
             str2[i + 1] = s2.charAt(i);
         }
 
-        for (int i = 1; i <= s1.length(); i++) {
-            for (int j = 1; j <= s2.length(); j++) {
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
                 if (str1[i] == str2[j]) {
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
@@ -35,10 +35,10 @@ public class Main {
             }
         }
 
-        lcs = dp[s1.length()][s2.length()];
+        lcs = dp[len1][len2];
         System.out.println(lcs);
         if (lcs != 0) {
-            System.out.println(searchLCS(s1.length(), s2.length(), new StringBuilder()));
+            System.out.println(searchLCS(len1, len2, new StringBuilder()));
         }
 
         br.close();
@@ -60,22 +60,4 @@ public class Main {
         }
         return answer.reverse();
     }
-
-    // 시간 초과
-//    private static void searchLCS(int i, int j, String answer) {
-//        if (i < 1 || j < 1 || found) {
-//            if (!found && answer.length() == lcs) {
-//                found = true;
-//                sb.append(answer);
-//            }
-//            return;
-//        }
-//
-//        if (str1[i] == str2[j]) {
-//            searchLCS(i - 1, j - 1, answer + str1[i]);
-//        } else {
-//            searchLCS(i - 1, j, answer);
-//            searchLCS(i, j - 1, answer);
-//        }
-//    }
 }
