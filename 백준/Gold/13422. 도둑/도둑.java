19a6aca0336dcb4fa644cb7d2603547badcd4c1a
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-04-13 10:42:49 +0900

    [Gold IV] Title: 도둑, Time: 736 ms, Memory: 100972 KB -BaekjoonHub

---
+++ 백준/Gold/13422. 도둑/도둑.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            int[] home = new int[n];
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                home[j] = Integer.parseInt(st.nextToken());
+            }
+
+            int cnt = 0;
+            int sum = 0;
+            for (int j = 0; j < m; j++) {
+                sum += home[j];
+            }
+            if (sum < k) cnt++;
+            for (int j = 1; j < n; j++) {
+                sum -= home[j - 1];
+                sum += home[(j + m - 1) % n];
+                if (sum < k) {
+                    if ((j - 1) != ((j + m - 1) % n)) {
+                        cnt++;
+                    }
+                }
+            }
+            sb.append(cnt).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}
