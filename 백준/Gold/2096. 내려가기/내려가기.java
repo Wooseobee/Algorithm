fca514d6abe2c089b556dbec1b51987db74bc731
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-04-14 12:11:37 +0900

    [Gold V] Title: 내려가기, Time: 436 ms, Memory: 49340 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -6,28 +6,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][3];
+
         int[][] maxDp = new int[n][3];
         int[][] minDp = new int[n][3];
 
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
-            arr[i][2] = Integer.parseInt(st.nextToken());
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 3; i++) {
-            maxDp[0][i] = arr[0][i];
-            minDp[0][i] = arr[0][i];
+            int num = Integer.parseInt(st.nextToken());
+            maxDp[0][i] = num;
+            minDp[0][i] = num;
         }
 
         for (int i = 1; i < n; i++) {
-            maxDp[i][0] = arr[i][0] + Math.max(maxDp[i - 1][0], maxDp[i - 1][1]);
-            maxDp[i][1] = arr[i][1] + Math.max(maxDp[i - 1][0], Math.max(maxDp[i - 1][1], maxDp[i - 1][2]));
-            maxDp[i][2] = arr[i][2] + Math.max(maxDp[i - 1][1], maxDp[i - 1][2]);
-            minDp[i][0] = arr[i][0] + Math.min(minDp[i - 1][0], minDp[i - 1][1]);
-            minDp[i][1] = arr[i][1] + Math.min(minDp[i - 1][0], Math.min(minDp[i - 1][1], minDp[i - 1][2]));
-            minDp[i][2] = arr[i][2] + Math.min(minDp[i - 1][1], minDp[i - 1][2]);
+            st = new StringTokenizer(br.readLine());
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+            int num3 = Integer.parseInt(st.nextToken());
+
+            maxDp[i][0] = num1 + Math.max(maxDp[i - 1][0], maxDp[i - 1][1]);
+            maxDp[i][1] = num2 + Math.max(maxDp[i - 1][0], Math.max(maxDp[i - 1][1], maxDp[i - 1][2]));
+            maxDp[i][2] = num3 + Math.max(maxDp[i - 1][1], maxDp[i - 1][2]);
+            minDp[i][0] = num1 + Math.min(minDp[i - 1][0], minDp[i - 1][1]);
+            minDp[i][1] = num2 + Math.min(minDp[i - 1][0], Math.min(minDp[i - 1][1], minDp[i - 1][2]));
+            minDp[i][2] = num3 + Math.min(minDp[i - 1][1], minDp[i - 1][2]);
         }
 
         System.out.println(Math.max(maxDp[n - 1][0], Math.max(maxDp[n - 1][1], maxDp[n - 1][2])) + " " + Math.min(minDp[n - 1][0], Math.min(minDp[n - 1][1], minDp[n - 1][2])));
