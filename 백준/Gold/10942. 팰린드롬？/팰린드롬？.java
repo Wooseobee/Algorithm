aac763a16cef2cd414e2f78d24121e32a82a43fc
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-04-21 17:35:07 +0900

    [Gold IV] Title: 팰린드롬?, Time: 812 ms, Memory: 219072 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -6,29 +6,40 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
+        boolean[][] dp = new boolean[n][n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i]==arr[i+1]) dp[i][i + 1] = true;
+        }
+
+        for (int i = 2; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                if (arr[j] == arr[j + i] && dp[j + 1][j + i - 1]) {
+                    dp[j][j + i] = true;
+                }
+            }
+        }
+
         int m = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken()) - 1;
             int e = Integer.parseInt(st.nextToken()) - 1;
-            if (checkPalindrome(arr, s, e)) sb.append(1).append("\n");
-            else sb.append(0).append("\n");
+            if (dp[s][e]) sb.append(1);
+            else sb.append(0);
+            sb.append("\n");
         }
+
         System.out.println(sb);
         br.close();
-    }
-
-    private static boolean checkPalindrome(int[] arr, int s, int e) {
-        while (s <= e) {
-            if (arr[s] != arr[e]) return false;
-            s++;
-            e--;
-        }
-        return true;
     }
 }
