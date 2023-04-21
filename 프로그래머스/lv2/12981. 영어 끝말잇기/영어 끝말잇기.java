afd199e2173e87457dce8603a2ea85a6860369f0
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-04-21 15:34:22 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -8,18 +8,18 @@ class Solution {
         word.add(words[0]);
         
         for(int i=1;i<words.length;i++){
-            if(word.contains(words[i])){
+            word.add(words[i]);
+            if(word.size()!=i+1){   // 중복 단어 확인
                 answer[0]=i%n+1;
                 answer[1]=i/n+1;
                 break;
             }
             char firstCharacter = words[i].charAt(0);
-            if(prev!=firstCharacter){
+            if(prev!=firstCharacter){   // 끝말과 시작말 확인
                 answer[0]=i%n+1;
                 answer[1]=i/n+1;
                 break;
             }
-            word.add(words[i]);
             prev = words[i].charAt(words[i].length()-1);
         }
 
