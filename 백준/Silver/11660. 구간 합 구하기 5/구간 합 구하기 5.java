5fbdba0d866fe81ced13bd114d7f6a24b444e7ba
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-04-23 18:17:33 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1368 ms, Memory: 114920 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -9,21 +9,12 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        int[][] arr = new int[n + 1][n + 1];
         int[][] dp = new int[n + 1][n + 1];
 
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= n; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        for (int i = 1; i <= n; i++) {
-            int sum = 0;
-            for (int j = 1; j <= n; j++) {
-                sum += arr[i][j];
-                dp[i][j] = sum;
+                dp[i][j] = dp[i][j - 1] + Integer.parseInt(st.nextToken());
             }
         }
 
