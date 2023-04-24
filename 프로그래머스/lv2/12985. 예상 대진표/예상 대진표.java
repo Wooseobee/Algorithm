c4ed2b95b502c64b8edbb9503c794285c3954c03
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-04-24 22:57:07 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -3,20 +3,10 @@ class Solution
     public int solution(int n, int a, int b)
     {
         int answer = 0;
-        int left = a < b ? a : b;
-        int right = a > b ? a : b;
-        while(n>0){
-            n/=2;
-            if(n!=0 && n<left && n<right){
-                left%=n;
-                right%=n;
-                if(right==0) right=n;
-            }
-            if(n<right) break;
-        }
         
-        while(n>0){
-            n/=2;
+        while(a != b) {
+            a = (a+1)/2;
+            b = (b+1)/2;
             answer++;
         }
 
