d4fa9d35efaa307d11af28631e8c1e8892730930
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-04-26 10:51:25 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 3608 ms, Memory: 350320 KB -BaekjoonHub

---
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Point {
+        int i;
+        int j;
+        int moveCount;
+        int brokenCount;
+
+        public Point(int i, int j, int moveCount, int brokenCount) {
+            this.i = i;
+            this.j = j;
+            this.moveCount = moveCount;
+            this.brokenCount = brokenCount;
+        }
+    }
+
+    static int n, m, k;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split("");
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(input[j]);
+            }
+        }
+
+        System.out.println(bfs());
+        br.close();
+    }
+
+    private static int bfs() {
+        PriorityQueue<Point> pq = new PriorityQueue<>((o1, o2) -> o1.moveCount - o2.moveCount);
+        boolean[][] visited = new boolean[n][m];
+        boolean[][][] brokenVisited = new boolean[n][m][k + 1]; // (n,m) k번 부순 길
+        pq.add(new Point(0, 0, 1, 0));
+        visited[0][0] = true;
+        brokenVisited[0][0][0] = true;
+
+        while (!pq.isEmpty()) {
+            Point now = pq.poll();
+            int breakCount = now.brokenCount;
+
+            if (now.i == n - 1 && now.j == m - 1) {
+                return now.moveCount;
+            }
+            for (int i = 0; i < 4; i++) {
+                int newI = now.i + dy[i];
+                int newJ = now.j + dx[i];
+                if (newI >= 0 && newJ >= 0 && newI < n && newJ < m) {
+                    if (arr[newI][newJ] == 0) { // 벽이 없을 때
+                        if (breakCount > 0) {  //이미 한 번 이상 벽을 부쉈을 때
+                            if (!brokenVisited[newI][newJ][breakCount]) {
+                                pq.add(new Point(newI, newJ, now.moveCount + 1, breakCount));
+                                brokenVisited[newI][newJ][breakCount] = true;
+                            }
+                        } else {
+                            if (!visited[newI][newJ]) {
+                                visited[newI][newJ] = true;
+                                pq.add(new Point(newI, newJ, now.moveCount + 1, breakCount));
+
+                            }
+                        }
+                    } else {    // 벽이 있을 때
+                        if (breakCount < k && !brokenVisited[newI][newJ][breakCount + 1]) {
+                            pq.add(new Point(newI, newJ, now.moveCount + 1, breakCount + 1));
+                            brokenVisited[newI][newJ][breakCount + 1] = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
