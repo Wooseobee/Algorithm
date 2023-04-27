da813c013515a9e03605c584a0365ab5769429db
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-04-27 16:41:08 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.77 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -0,0 +1,59 @@
+import java.util.*;
+class Solution {
+    static List<Character> openBracket = new ArrayList<>();
+    public int solution(String s) {
+        int answer = 0;
+        int len = s.length();
+
+        openBracket.add('[');
+        openBracket.add('{');
+        openBracket.add('(');
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for(int i=0;i<len;i++){
+            deque.add(s.charAt(i));
+        }
+
+        for(int i=0;i<len;i++){
+            deque.addLast(deque.removeFirst());
+            Deque<Character> tmp = new ArrayDeque<>(deque);
+            if(check(deque)) answer++;
+            deque = tmp;
+        }
+
+        return answer;
+    }
+
+    private static boolean check(Deque<Character> deque){
+        Stack<Character> stack = new Stack<>();
+
+        while(!deque.isEmpty()){
+            if(stack.isEmpty()) stack.push(deque.removeFirst());
+            else {
+                char top = stack.peek();
+                char first = deque.removeFirst();
+                switch(first){
+                    case '[':
+                    case '{':
+                    case '(':
+                        stack.push(first);
+                        break;
+                    case ']':
+                        if(top!='[') return false;
+                        stack.pop();
+                        break;
+                    case '}':
+                        if(top!='{') return false;
+                        stack.pop();
+                        break;
+                    case ')':
+                        if(top!='(') return false;
+                        stack.pop();
+                        break;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
