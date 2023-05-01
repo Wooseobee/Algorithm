f3e21d0521a6305c38d2358466c0cc4f25de3758
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-05-01 19:49:43 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.java
@@ -1,39 +1,16 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] arr) {
-        int answer = 1;
-        boolean[] notPrime = new boolean[101];
+        int answer = arr[0];
 
-        for (int i = 2; i <= 100; i++) {
-            if (!notPrime[i]) {
-                for (int j = i * i; j <= 100; j += i) {
-                    notPrime[j] = true;
-                }
-            }
-        }
-
-        for (int i = 2; i <= 100; i++) {
-            if (!notPrime[i]) {
-                int divCnt = 0;
-                int[] tmp = Arrays.copyOf(arr, arr.length);
-                for (int j = 0; j < arr.length; j++) {
-                    if (arr[j] % i == 0) {
-                        arr[j] /= i;
-                        divCnt++;
-                    }
-                }
-                if (divCnt > 1) {
-                    answer *= i;
-                    i--;
-                } else arr = Arrays.copyOf(tmp, tmp.length);
-            }
-        }
-
-        for (int i = 0; i < arr.length; i++) {
-            answer *= arr[i];
+        for (int i = 1; i < arr.length; i++) {
+            answer = answer * arr[i] / GCD(answer, arr[i]);
         }
 
         return answer;
+    }
+
+    private static int GCD(int a, int b) {
+        if (b == 0) return a;
+        return GCD(b, a % b);
     }
 }
