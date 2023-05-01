23a70b9392386e3b26d07127448a14e5df0ad277
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-05-02 01:05:22 +0900

    [level 2] Title: 괄호 회전하기, Time: 0.20 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/lv2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,49 +1,49 @@
 import java.util.*;
+
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        int len = s.length();
 
-        Deque<Character> deque = new ArrayDeque<>();
+        char[] arr = s.toCharArray();
+        int len = arr.length;
 
-        for(int i=0;i<len;i++){
-            deque.add(s.charAt(i));
-        }
+        for (int i = 0; i < len; i++) {
+            char ch = arr[0];
+            for (int j = 1; j < len; j++) {
+                arr[j - 1] = arr[j];
+            }
+            arr[len - 1] = ch;
+            if (check(arr)) answer++;
 
-        for(int i=0;i<len;i++){
-            deque.addLast(deque.removeFirst());
-            Deque<Character> tmp = new ArrayDeque<>(deque);
-            if(check(deque)) answer++;
-            deque = tmp;
         }
 
         return answer;
     }
 
-    private static boolean check(Deque<Character> deque){
+    private static boolean check(char[] arr) {
         Stack<Character> stack = new Stack<>();
 
-        while(!deque.isEmpty()){
-            if(stack.isEmpty()) stack.push(deque.removeFirst());
+        for (int i = 0; i < arr.length; i++) {
+            if (stack.isEmpty()) stack.push(arr[i]);
             else {
                 char top = stack.peek();
-                char first = deque.removeFirst();
-                switch(first){
+                char first = arr[i];
+                switch (first) {
                     case '[':
                     case '{':
                     case '(':
                         stack.push(first);
                         break;
                     case ']':
-                        if(top!='[') return false;
+                        if (top != '[') return false;
                         stack.pop();
                         break;
                     case '}':
-                        if(top!='{') return false;
+                        if (top != '{') return false;
                         stack.pop();
                         break;
                     case ')':
-                        if(top!='(') return false;
+                        if (top != '(') return false;
                         stack.pop();
                         break;
                 }
