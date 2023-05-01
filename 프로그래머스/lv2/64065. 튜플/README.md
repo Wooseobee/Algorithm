a97c6419184b60ba2ac8e6aad84372a629882a29
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-05-01 19:45:46 +0900

    [level 2] Title: 튜플, Time: 2.67 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv2/64065. 튜플/README.md
+++ 프로그래머스/lv2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
 
 ### 성능 요약
 
-메모리: 80 MB, 시간: 2.68 ms
+메모리: 79.1 MB, 시간: 2.67 ms
 
 ### 구분
 
--- 프로그래머스/lv2/64065. 튜플/튜플.java
+++ 프로그래머스/lv2/64065. 튜플/튜플.java
@@ -2,22 +2,18 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        s = s.replaceAll("^[{{]|[}}]$", "");
-        s = s.replaceAll("^[{]|[}]$", "");
-        String[] strings = s.split("[}]{1}[,]{1}[{]{1}");
+        String[] strings = s.replaceAll("^[{]{2}|[}]{2}$", "").split("[}]{1}[,]{1}[{]{1}");
 
         PriorityQueue<String> pq = new PriorityQueue<>((o1, o2) -> o1.length() - o2.length());
-        for (int i = 0; i < strings.length; i++) {
-            pq.add(strings[i]);
-        }
+        pq.addAll(Arrays.asList(strings));
 
         List<Integer> ans = new ArrayList<>();
 
         while (!pq.isEmpty()) {
             String str = pq.poll();
-            
+
             String[] strs = str.split(",");
-            
+
             for(String ss : strs){
                 if (!ans.contains(Integer.parseInt(ss))) {
                     ans.add(Integer.parseInt(ss));
