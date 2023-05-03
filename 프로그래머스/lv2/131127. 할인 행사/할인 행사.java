4647b3635c3432a3fe426ac63c8a089261eb0401
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-05-03 18:17:21 +0900

    [level 2] Title: 할인 행사, Time: 0.06 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -4,28 +4,24 @@ class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         int len = number.length;
-        Map<String, Integer> product = new HashMap<>();
 
         for (int i = 0; i < discount.length - len; i++) {
-            for (int j = 0; j < len; j++) {
-                product.put(want[j], number[j]);
-            }
+            Map<String, Integer> discountProduct = new HashMap<>();
             for (int j = 0; j < 10; j++) {
                 if (i + j == discount.length) break;
                 String todayFood = discount[i + j];
-                if (product.containsKey(todayFood)) {
-                    product.put(todayFood, product.get(todayFood) - 1);
-                }
+                discountProduct.put(todayFood, discountProduct.getOrDefault(todayFood, 0) + 1);
             }
-            boolean canRegist = true;
-            for (int value : product.values()) {
-                if (value != 0) {
-                    canRegist = false;
-                    break;
-                }
-            }
-            if (canRegist) answer++;
+            if (canRegist(want, number, discountProduct)) answer++;
         }
         return answer;
+    }
+
+    private static boolean canRegist(String[] want, int[] number, Map<String, Integer> discountProduct) {
+        for (int i = 0; i < want.length; i++) {
+            String product = want[i];
+            if (!discountProduct.containsKey(product) || discountProduct.get(product) < number[i]) return false;
+        }
+        return true;
     }
 }
