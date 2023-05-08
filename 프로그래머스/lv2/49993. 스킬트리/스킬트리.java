a05d9462b01f8d918f2603c08898f6496a284651
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-05-08 23:45:23 +0900

    [level 2] Title: 스킬트리, Time: 0.12 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -39,4 +39,14 @@ class Solution {
         }
         return true;
     }
+    
+    // // 정규표현식 + indexOf 이용
+    // public int solution(String skill, String[] skill_trees) {
+    //     int answer = 0;
+    //     for (String s : skill_trees) {
+    //         s = s.replaceAll("[^" + skill + "]", "");
+    //         if (skill.indexOf(s) == 0) answer++;
+    //     }
+    //     return answer;
+    // }
 }
