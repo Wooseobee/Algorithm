6d7b72f02034de3520541f9fff59b579b1a1f5f3
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-05-16 20:43:16 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 88.89 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.java
@@ -10,24 +10,8 @@ class Solution {
     private static long func(long num) {
         if (num % 2 == 0) return num + 1;   // 짝수면 +1하고 반환
 
-        String s = Long.toBinaryString(num);
-        StringBuilder sb = new StringBuilder();
-        int j = 0;
-        boolean found = false;
-        for (int i = s.length() - 1; i >= 0; i--, j++) {
-            if (s.charAt(i) == '0' && !found) {     // 현재 숫자가 0이면 현재 숫자 1로 바꾸고 바로 이전 숫자 0으로 바꾸기 ex) 11011 => 11101, 110011 => 110101
-                sb.setCharAt(j - 1, '0');
-                sb.append("1");
-                found = true;
-                continue;
-            }
-            sb.append(s.charAt(i));
-        }
-        if (!found) {   // 모두 1이면 앞에 1 추가하고 바로 이전 숫자 0으로 바꾸기 ex) 111 => 1011
-            sb.setCharAt(j - 1, '0');
-            sb.append("1");
-
-        }
-        return Long.parseLong(sb.reverse().toString(), 2);
+        String s = "0" + Long.toBinaryString(num);
+        int idx = s.lastIndexOf("0");
+        return Long.parseLong(s.substring(0, idx) + "10" + s.substring(idx + 2), 2);
     }
 }
