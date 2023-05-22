39563d058f4fb40ccef1a85214c062c3efcbe4e4
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-05-22 23:04:44 +0900

    [level 2] Title: 택배상자, Time: 183.86 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/lv2/131704. 택배상자/택배상자.java
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -6,31 +6,14 @@ class Solution {
 
         Stack<Integer> stack = new Stack<>();
 
-        int i = 0;
-        for (int j = 1; i < order.length && j <= order.length; j++) {
-            if (order[i] == j) {
+        for (int j = 1; j <= order.length; j++) {
+            stack.push(j);
+            while(!stack.isEmpty() && stack.peek() == order[answer]){
                 answer++;
-                i++;
-            } else {
-                if (!stack.isEmpty() && stack.peek() == order[i]) {
-                    answer++;
-                    i++;
-                    stack.pop();
-                    j--;
-                } else {
-                    stack.push(j);
-                }
-            }
-        }
-        while (!stack.isEmpty()) {
-            if (order[i] == stack.peek()) {
-                answer++;
-                i++;
                 stack.pop();
-            } else {
-                break;
             }
         }
+        
         return answer;
     }
 }
