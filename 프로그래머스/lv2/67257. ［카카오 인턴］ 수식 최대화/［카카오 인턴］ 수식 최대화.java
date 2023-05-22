de022b26a6e4fc84f098829709ff1f5e0004060d
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-05-23 01:50:46 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 4.29 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/lv2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -2,48 +2,25 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 class Solution {
-    static char[] priority;
-    static List<Character> opList = new ArrayList<>();
-    static boolean[] visited;
+    static String[] priorities = {"*+-", "*-+", "+-*", "+*-", "-+*", "-*+"};
     static long max = Long.MIN_VALUE;
 
     public long solution(String expression) {
         String operators = expression.replaceAll("[0-9]", "");
 
-        for (int i = 0; i < operators.length(); i++) {
-            if (!opList.contains(operators.charAt(i))) opList.add(operators.charAt(i));
+        for (String priority : priorities) {
+            calculate(expression, priority);
         }
-
-        priority = new char[opList.size()];
-        visited = new boolean[opList.size()];
-
-        makePriority(0, opList.size(), expression);
         return max;
     }
 
-    private static void makePriority(int depth, int size, String expression) {
-        if (depth == priority.length) {
-            calculate(expression);
-            return;
-        }
-        for (int i = 0; i < size; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-
-                priority[depth] = opList.get(i);
-                makePriority(depth + 1, size, expression);
-
-                visited[i] = false;
-            }
-        }
-    }
-
-    private static void calculate(String expression) {
+    private static void calculate(String expression, String priority) {
         List<String> operands = Arrays.stream(expression.split("[*+-]")).collect(Collectors.toList());
         List<String> operators = Arrays.stream(expression.split("[0-9]+")).collect(Collectors.toList());
         operators.remove(0);
 
-        for (char operator : priority) {
+        for (int i = 0; i < 3; i++) {
+            char operator = priority.charAt(i);
             for (int j = 0; j < operators.size(); j++) {
                 char now = operators.get(j).charAt(0);
                 if (now == operator) {
