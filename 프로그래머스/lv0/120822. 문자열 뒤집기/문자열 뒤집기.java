c08d54c680441699345379dbb451d1e8c937c0e5
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-05-28 10:56:17 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/lv0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,7 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<my_string.length();i++) sb.append(my_string.charAt(i));
-        return sb.reverse().toString();
+        return new StringBuilder(my_string).reverse().toString();
     }
 }
