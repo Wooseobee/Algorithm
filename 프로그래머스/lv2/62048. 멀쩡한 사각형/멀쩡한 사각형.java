38bbbeb66733b584b6319e171b6b5d033f5b8962
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-05-31 23:52:10 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.06 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/lv2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,10 +1,17 @@
 class Solution {
-    public static long solution(int w, int h) {
-        long answer = 0;
-        for (long i = 1; i < w; i++) {
-            long height = (long) h * i / w;
-            answer += height;
+    public long solution(int w, int h) {
+        long gcd = getGcd(w, h);
+        long w1 = w / gcd;
+        long h1 = h / gcd;
+        long cnt = 0;
+        for (long i = 1; i < w1; i++) {
+            cnt += (long) h1 * i / w1;
         }
-        return answer * 2;
+        return (long) w * h - (gcd * ((w1 * h1) - (cnt * 2)));
+    }
+
+    private static long getGcd(int a, int b) {
+        if (b == 0) return a;
+        return getGcd(b, a % b);
     }
 }
