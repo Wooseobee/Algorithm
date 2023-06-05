79a49cd0937f19e0d7e531d99cad299628d67ad9
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-06-06 03:02:19 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.09 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -1,10 +1,5 @@
 class Solution {
     public String solution(int n) {
-        String s = "";
-        for(int i = 1;i <= n;i++){
-            if(i % 2 == 1) s += "수";
-            else s += "박";
-        }
-        return s;
+        return "수박".repeat(n / 2 + 1).substring(0, n);
     }
 }
