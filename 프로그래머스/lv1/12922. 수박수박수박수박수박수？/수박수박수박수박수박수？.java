e9fbed1d2bace3d43b0b06f46bd84ac331ddecac
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-06-06 01:16:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 2.72 ms, Memory: 79.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        String s = "";
+        for(int i = 1;i <= n;i++){
+            if(i % 2 == 1) s += "수";
+            else s += "박";
+        }
+        return s;
+    }
+}
