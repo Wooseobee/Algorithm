137f4d2938743206c99594bd5d327947fa78771f
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-06-06 02:34:41 +0900

    [unrated] Title: 우박수열 정적분, Time: 3.22 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/unrated/134239. 우박수열 정적분/우박수열 정적분.java
+++ 프로그래머스/unrated/134239. 우박수열 정적분/우박수열 정적분.java
@@ -3,24 +3,24 @@ import java.util.*;
 class Solution {
     public double[] solution(int k, int[][] ranges) {
         double[] answer = new double[ranges.length];
-        double[] area = setArea(k);
+        List<Integer> areaList = setAreaList(k);
 
         for (int i = 0; i < ranges.length; i++) {
             int a = ranges[i][0];
-            int b = area.length + ranges[i][1];
-            answer[i] = getArea(a, b, area);
+            int b = areaList.size() + ranges[i][1] - 1;
+            answer[i] = getArea(a, b, areaList);
         }
         return answer;
     }
 
-    private double getArea(int a, int b, double[] area) {
+    private double getArea(int a, int b, List<Integer> areaList) {
         if (a > b) return -1.0;
         double total = 0;
-        for (int i = a; i < b; i++) total += area[i];
+        for (int i = a; i < b; i++) total += (areaList.get(i) + areaList.get(i + 1)) / 2.0;
         return total;
     }
 
-    private double[] setArea(int k) {
+    private List<Integer> setAreaList(int k) {
         List<Integer> areaList = new ArrayList<>();
         while (k > 1) {
             areaList.add(k);
@@ -29,15 +29,6 @@ class Solution {
         }
         areaList.add(1);
 
-        int len = areaList.size();
-        double[] area = new double[len - 1];
-
-        for (int i = 0; i < len - 1; i++) {
-            double v = Math.min(areaList.get(i), areaList.get(i + 1)) + Math.abs(areaList.get(i) - areaList.get(i + 1)) / 2.0;
-            v = v * 10 / 10;
-            area[i] = v;
-        }
-
-        return area;
+        return areaList;
     }
 }
