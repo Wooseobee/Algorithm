70bf6f13d2c3a6663219c73c6dc69977fc2f53a5
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-06-07 22:59:18 +0900

    [level 1] Title: 예산, Time: 0.39 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.java
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -1,15 +1,11 @@
 import java.util.*;
 class Solution {
     public int solution(int[] d, int budget) {
-        int answer = 0;
-        d = Arrays.stream(d)
-                .boxed().sorted()
-                .mapToInt(Integer::intValue).toArray();
+        Arrays.sort(d);
         for (int i = 0; i < d.length; i++) {
             budget -= d[i];
-            answer++;
             if (budget < 0) return i;
         }
-        return answer;
+        return d.length;
     }
 }
