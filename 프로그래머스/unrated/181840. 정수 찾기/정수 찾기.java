4430cb63e5b8c65eb4bf7aa6f6b0e97567b454ed
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-06-12 09:40:10 +0900

    [unrated] Title: 정수 찾기, Time: 1.86 ms, Memory: 66 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] num_list, int n) {
+        return Arrays.stream(num_list).boxed().collect(Collectors.toList()).contains(n) ? 1 : 0;
+    }
+}
