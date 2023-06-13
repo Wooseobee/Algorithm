771cb786ab6c02249814a662937c7f59fa36578f
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-06-13 17:09:10 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 8.47 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,6 @@
+import java.util.stream.IntStream;
+class Solution {
+    public String solution(String[] seoul) {
+        return "김서방은 " + IntStream.rangeClosed(0, seoul.length).filter(i -> seoul[i].equals("Kim")).findFirst().orElse(0) + "에 있다";
+    }
+}
