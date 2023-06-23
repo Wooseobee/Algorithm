21f270ff31ac7bf97af60dd2ca91f113ae931d3a
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-06-23 09:10:26 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.66 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,12 +1,7 @@
 import java.util.*;
-import java.util.stream.Collectors;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        List<Integer> list = Arrays.stream(numbers).boxed().collect(Collectors.toList());
-        for (int i = 1; i <= 9; i++) {
-            if (!list.contains(i)) answer += i;
-        }
-        return answer;
+        return 45 - Arrays.stream(numbers).sum();
     }
 }
