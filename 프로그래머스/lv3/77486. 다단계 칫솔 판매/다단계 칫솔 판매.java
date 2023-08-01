60f087108b5e34b56b149662245ef99539590a33
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-08-02 08:30:10 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 35.34 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class Solution {
+    public static int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+		int n = enroll.length;
+		int sell = seller.length;
+		int[] answer = new int[n];
+
+		Map<String, String> parentMap = new HashMap<>();
+		Map<String, Integer> idxMap = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			idxMap.put(enroll[i], i);
+			parentMap.put(enroll[i], referral[i]);
+		}
+
+		for (int i = 0; i < sell; i++) {
+			String parent = parentMap.get(seller[i]);
+			int forMyMoney = amount[i] * 90;
+			int forParentMoney = amount[i] * 10;
+
+			answer[idxMap.get(seller[i])] += forMyMoney;
+
+			while (!parent.equals("-") || forParentMoney >= 1) {
+				if (forParentMoney * 10 / 100 < 1) {
+					forMyMoney = forParentMoney;
+					forParentMoney = 0;
+				} else {
+					int tmp = forParentMoney;
+					forParentMoney = forParentMoney * 10 / 100;
+					forMyMoney = tmp - forParentMoney;
+				}
+				if (!parent.equals("-")) {
+					answer[idxMap.get(parent)] += forMyMoney;
+				}
+				parent = parentMap.getOrDefault(parent, "-");
+                if(forParentMoney == 0) break;
+			}
+		}
+
+		return answer;
+	}
+}
