a6b06b60019a5f73b60949f5acd21d03436c4092
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-08-02 12:15:19 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 964 ms, Memory: 147736 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,38 +1,38 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
 
-        int[][] dp = new int[n + 1][n + 1];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String[] in = br.readLine().split(" ");
+		int n = Integer.parseInt(in[0]);
+		int m = Integer.parseInt(in[1]);
 
-        for (int i = 1; i <= n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 1; j <= n; j++) {
-                dp[i][j] = dp[i][j - 1] + Integer.parseInt(st.nextToken());
-            }
-        }
+		int[][] arr = new int[n + 1][n + 1];
+		int[][] dp = new int[n + 1][n + 1];
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x1 = Integer.parseInt(st.nextToken());
-            int y1 = Integer.parseInt(st.nextToken());
-            int x2 = Integer.parseInt(st.nextToken());
-            int y2 = Integer.parseInt(st.nextToken());
-            int sum = 0;
-            for (int j = x1; j <= x2; j++) {
-                sum += dp[j][y2] - dp[j][y1 - 1];
-            }
-            sb.append(sum).append("\n");
-        }
+		for (int i = 1; i <= n; i++) {
+			in = br.readLine().split(" ");
+			for (int j = 1; j <= n; j++) {
+				arr[i][j] = Integer.parseInt(in[j - 1]);
+				dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
+			}
+		}
 
-        System.out.println(sb);
-        br.close();
-    }
+		for (int i = 0; i < m; i++) {
+			in = br.readLine().split(" ");
+			int x1 = Integer.parseInt(in[0]);
+			int y1 = Integer.parseInt(in[1]);
+			int x2 = Integer.parseInt(in[2]);
+			int y2 = Integer.parseInt(in[3]);
+
+			int sum = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1];
+			sb.append(sum).append("\n");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+
 }
