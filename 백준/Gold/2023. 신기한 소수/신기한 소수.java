425a5834c8af588d394175c5ae81d76c58539936
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-08-03 15:30:24 +0900

    [Gold V] Title: 신기한 소수, Time: 132 ms, Memory: 14068 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,43 +1,37 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+	private static StringBuilder sb = new StringBuilder();
+	private static int[] prime = { 2, 3, 5, 7 };
+
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 
 		int n = Integer.parseInt(br.readLine());
 
-		for (int i = (int) Math.pow(10, n - 1) * 2; i < Math.pow(10, n - 1) * 8; i++) {
-			if (isSpecial(i, n))
-				sb.append(i).append("\n");
+		for (int i = 0; i < prime.length; i++) {
+			backTracking(prime[i], n - 1);
 		}
 		System.out.println(sb);
 		br.close();
 	}
-	
-	private static boolean isSpecial(int i, int n) {
-		int idx = (int) Math.pow(10, n - 1);
-		int cnt = 0;
-		while (idx > 0) {
-			int num = i / idx;
-			if (!checkNum(num)) {
-				return false;
-			}
-			if (cnt > 0 && !(num % 10 == 1 || num % 10 == 3 || num % 10 == 7 || num % 10 == 9)) {
-				return false;
-			}
-			cnt++;
-			idx /= 10;
-		}
 
-		return true;
+	private static void backTracking(int num, int depth) {
+		if (depth == 0) {
+			sb.append(num).append("\n");
+			return;
+		}
+		for (int i = 1; i < 10; i += 2) {
+			if (isPrime(num * 10 + i)) {
+				backTracking(num * 10 + i, depth - 1);
+			}
+		}
 	}
 
-	private static boolean checkNum(int n) {
-		if (n < 2 || (n < 10 && !(n == 2 || n == 3 || n == 5 || n == 7))) return false;
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			if (n % i == 0)
-				return false;
+	private static boolean isPrime(int num) {
+		if (num < 2) return false;
+		for (int i = 2; i <= Math.sqrt(num); i++) {
+			if (num % i == 0) return false;
 		}
 		return true;
 	}
