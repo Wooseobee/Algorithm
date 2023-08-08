48fe4cbb1b51df1fefd58a420c8379628974ddb6
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-08-08 15:17:27 +0900

    [D4] Title: [S/W 문제해결 기본] 9일차 - 사칙연산 유효성 검사, Time: 179 ms, Memory: 26,236 KB -BaekjoonHub

--- SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
+++ SWEA/D4/1233. ［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사/［S／W 문제해결 기본］ 9일차 － 사칙연산 유효성 검사.java
@@ -8,29 +8,30 @@ public class Solution {
 		int T = 10;
 		for (int test_case = 1; test_case <= T; test_case++) {
 			int n = Integer.parseInt(sc.nextLine());
-			char[] tree = new char[n+1];
-			List<Integer> list = new ArrayList<>();
+			char[] tree = new char[n + 1];
 			String[] in;
-			for(int i = 1; i<=n;i++) {
+			for (int i = 1; i <= n; i++) {
 				in = sc.nextLine().split(" ");
 				int idx = Integer.parseInt(in[0]);
 				tree[idx] = in[1].charAt(0);
-				if (Character.isDigit(tree[idx])) {
-					list.add(idx);
-				}
 			}
-			
-			int ans = checkCanCal(list, n) ? 1 : 0;
-			
+
+			int ans = checkCanCal(tree, n) ? 1 : 0;
+
 			sb.append("#").append(test_case).append(" ").append(ans).append("\n");
 		}
 		System.out.println(sb);
 		sc.close();
 	}
-    
-	private static boolean checkCanCal(List<Integer> list, int n) {
-		for(int idx : list) {
-			if(idx * 2 <= n) return false;
+
+	private static boolean checkCanCal(char[] tree, int n) {
+		for (int i = 1; i <= n; i++) {
+			char ch = tree[i];
+			if (Character.isDigit(ch)) {
+				if (i * 2 <= n) return false;
+			} else {
+				if (i * 2 > n) return false;
+			}
 		}
 		return true;
 	}
