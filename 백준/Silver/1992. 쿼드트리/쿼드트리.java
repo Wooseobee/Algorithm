363d108d06e1b3905f02f1c22042440ca62b1e98
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-08-16 15:29:48 +0900

    [Silver I] Title: 쿼드트리, Time: 148 ms, Memory: 15532 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,55 +1,52 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    static int[][] video;
-    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static StringBuilder sb = new StringBuilder();
+	private static int n;
+	private static int[][] video;
 
-        int n = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        video = new int[n][n];
+		n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split("");
-            for (int j = 0; j < n; j++) {
-                video[i][j] = Integer.parseInt(input[j]);
-            }
-        }
+		video = new int[n][n];
 
-        if (checkVideo(0, 0, n)) {
-            if (video[0][0] == 0) System.out.println(0);
-            else System.out.println(1);
-        } else {
-            solution(0, 0, n);
-        }
-        System.out.println(sb);
-        br.close();
-    }
+		for (int i = 0; i < n; i++) {
+			String[] in = br.readLine().split("");
+			for (int j = 0; j < n; j++) {
+				video[i][j] = Integer.parseInt(in[j]);
+			}
+		}
 
-    static void solution(int i, int j, int size) {
-        if (checkVideo(i, j, size)) {
-            if (video[i][j]==0) sb.append("0");
-            else sb.append("1");
-            return;
-        }
-        sb.append("(");
-        solution(i, j, size / 2);
-        solution(i, j + size / 2, size / 2);
-        solution(i + size / 2, j, size / 2);
-        solution(i + size / 2, j + size / 2, size / 2);
-        sb.append(")");
-    }
+		compression(0, 0, n);
+		System.out.println(sb);
+		br.close();
+	}
 
-    static boolean checkVideo(int i, int j, int size) {
-        int color = video[i][j];
-        for (int k = i; k < i + size; k++) {
-            for (int l = j; l < j + size; l++) {
-                if (video[k][l]!=color) return false;
-            }
-        }
-        return true;
-    }
+	private static void compression(int i, int j, int size) {
+		if (canCompress(i, j, size)) {
+			sb.append(video[i][j]);
+			return;
+		}
+		int half = size / 2;
+		sb.append("(");
+		compression(i, j, half);
+		compression(i, j + half, half);
+		compression(i + half, j, half);
+		compression(i + half, j + half, half);
+		sb.append(")");
+	}
+
+	private static boolean canCompress(int i, int j, int size) {
+		int v = video[i][j];
+		for (int k = i; k < i + size; k++) {
+			for (int l = j; l < j + size; l++) {
+				if (video[k][l] != v)
+					return false;
+			}
+		}
+		return true;
+	}
 }
