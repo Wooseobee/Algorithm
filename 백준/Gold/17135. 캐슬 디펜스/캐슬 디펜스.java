0c45b01e06726916ece845475b36f92b261e24ee
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-08-18 16:56:28 +0900

    [Gold III] Title: 캐슬 디펜스, Time: 652 ms, Memory: 67684 KB -BaekjoonHub

--- 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
+++ 백준/Gold/17135. 캐슬 디펜스/캐슬 디펜스.java
@@ -1,15 +1,11 @@
 import java.io.*;
 import java.util.*;
 
-/*
- * 메모리 : 100,056 kb
- * 실행시간 : 880 ms
- */
 public class Main {
 
-	private static int n, m, d, max = 0;	// 격자판, 사거리, 최대 제거가능 수
-	private static int[] archer = new int[3];	// 궁수 3명
-	private static int[][] map;	// 격자판
+	private static int n, m, d, max = 0; // 격자판, 사거리, 최대 제거가능 수
+	private static int[] archer = new int[3]; // 궁수 3명
+	private static int[][] map; // 격자판
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,7 +16,7 @@ public class Main {
 		m = Integer.parseInt(in[1]);
 		d = Integer.parseInt(in[2]);
 
-		map = new int[n + 1][m];	// 격자판 초기화 및 세팅
+		map = new int[n + 1][m]; // 격자판 초기화 및 세팅
 
 		for (int i = 0; i < n; i++) {
 			in = br.readLine().split(" ");
@@ -34,7 +30,7 @@ public class Main {
 		br.close();
 	}
 
-	// 궁수의 위치 조합
+	// 궁수의 위치 조합 => 순서 상관없이 m개 중 3개 뽑기
 	private static void setArcher(int depth, int idx) {
 		if (depth == 3) {
 			playGame();
@@ -48,11 +44,16 @@ public class Main {
 
 	// 적들은 가만히, 궁수가 한칸씩 위로 이동
 	private static void playGame() {
-		Queue<int[]> q = new ArrayDeque<int[]>(); 
-		Set<String> killList = new HashSet<>();	// 죽인 적의 위치 저장할 set
-		int[][] tmpMap = new int[n][m];	// 복사할 격자판
+		Queue<int[]> q = new ArrayDeque<int[]>();
+		Set<String>[] killList = new HashSet[n + 1];
+		int kill = 0;
+		int[][] tmpMap = new int[n][m]; // 복사할 격자판
 		for (int i = 0; i < n; i++) {
 			tmpMap[i] = Arrays.copyOf(map[i], m);
+		}
+		
+		for (int i = 0; i <= n; i++) {
+			killList[i] = new HashSet<String>();
 		}
 
 		// 초기 궁수 위치 넣기
@@ -63,17 +64,17 @@ public class Main {
 		while (!q.isEmpty()) {
 
 			int[] now = q.poll();
-			
+
 			// 현재 궁수 위치
 			int i = now[0];
 			int j = now[1];
-			int order = now[2]; // 아처 순번
-			int round = now[3];	// 몇번 째 경기인지
+			int order = now[2]; // 궁수 순번
+			int round = now[3]; // 몇번째 경기인지
 
 			// 적을 죽일 수 있는 위치 초기 == 현재 궁수 위치 바로 위
 			int nI = i - 1;
 			int nJ = j;
-			int dist = 1;	// 거리 == 1
+			int dist = 1; // 거리 == 1
 
 			// 궁수가 공격하는 우선순위 : 가장 가까운 적 -> 가장 왼쪽의 적
 			PriorityQueue<int[]> pq = new PriorityQueue<int[]>((o1, o2) -> {
@@ -83,21 +84,21 @@ public class Main {
 				return o1[2] - o2[2];
 			});
 
-			// 궁수가 죽일 수 있는 적의 위치는 격자판 안에 있으며, 거리<=사거리 
+			// 궁수가 죽일 수 있는 적의 위치는 격자판 안에 있으며, 거리<=사거리
 			while (nI >= 0 && nJ < m && dist <= d) {
 				int tmpDist = dist;
-				
+
 				// 좌우로 탐색
 				for (int k = 0; tmpDist <= d; k++) {
-					if (nJ - k >= 0 && tmpMap[nI][nJ - k] == 1) {	// 왼쪽 탐색
+					if (nJ - k >= 0 && tmpMap[nI][nJ - k] == 1) { // 왼쪽 탐색
 						pq.add(new int[] { nI, nJ - k, tmpDist });
 					}
-					if (nJ + k < m && tmpMap[nI][nJ + k] == 1) {	// 오른쪽 탐색
+					if (nJ + k < m && tmpMap[nI][nJ + k] == 1) { // 오른쪽 탐색
 						pq.add(new int[] { nI, nJ + k, tmpDist });
 					}
 					tmpDist = Math.abs(i - nI) + Math.abs(j - nJ) + k + 1;
 				}
-				
+
 				// 한 칸 더 위의 적 탐색, 거리++
 				nI--;
 				dist++;
@@ -111,17 +112,19 @@ public class Main {
 			// 적의 위치 추출
 			if (!pq.isEmpty()) {
 				int[] killed = pq.poll();
-				killList.add(killed[0] + " " + killed[1]);
+				killList[round].add(killed[0] + " " + killed[1]);
 			}
 
 			// 한 라운드가 끝날 때마다 죽인 적의 위치 0으로 세팅
 			if (order == 2) {
-				for (String kL : killList) {
+				Set<String> finish = killList[round];
+				for (String kL : finish) {
 					String[] coord = kL.split(" ");
 					tmpMap[Integer.parseInt(coord[0])][Integer.parseInt(coord[1])] = 0;
 				}
+				kill+= finish.size();
 			}
 		}
-		max = Math.max(max, killList.size());
+		max = Math.max(max, kill);
 	}
 }
