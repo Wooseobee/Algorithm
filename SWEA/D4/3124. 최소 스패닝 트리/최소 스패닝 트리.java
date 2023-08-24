9c8f9824e78fea09c8661f97700284cf29852ff0
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2023-08-24 13:49:28 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,926 ms, Memory: 229,600 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -3,7 +3,15 @@ import java.util.*;
 
 public class Solution {
 
-	private static int[] parents;
+	private static class Node {
+		int v;
+		int w;
+
+		public Node(int v, int w) {
+			this.v = v;
+			this.w = w;
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,56 +24,48 @@ public class Solution {
 
 			int v = Integer.parseInt(in[0]);
 			int e = Integer.parseInt(in[1]);
-			int cnt = 0;
 			long total = 0;
 
-			parents = new int[v + 1];
+			boolean[] visited = new boolean[v + 1];
+
+			List[] nodes = new List[v + 1];
 			for (int i = 1; i <= v; i++) {
-				parents[i] = i;
+				nodes[i] = new ArrayList<Node>();
 			}
-			PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
 			for (int i = 0; i < e; i++) {
 				in = br.readLine().split(" ");
 				int a = Integer.parseInt(in[0]);
 				int b = Integer.parseInt(in[1]);
 				int c = Integer.parseInt(in[2]);
-				pq.add(new int[] { a, b, c });
+
+				nodes[a].add(new Node(b, c));
+				nodes[b].add(new Node(a, c));
 			}
 
+			PriorityQueue<Node> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1.w, o2.w));
+			pq.add(new Node(1, 0));
+			int cnt = 0;
 			while (!pq.isEmpty()) {
-				int[] now = pq.poll();
-				int a = now[0];
-				int b = now[1];
-				int c = now[2];
+				Node now = pq.poll();
+				int a = now.v;
+				int c = now.w;
 
-				if (union(a, b)) {
-					total += c;
-					if (++cnt == v - 1)
-						break;
+				if (visited[a]) continue;
+				total += c;
+				visited[a] = true;
+				if(++cnt == v) break;
+
+				for (Node next : (ArrayList<Node>) nodes[a]) {
+					if (!visited[next.v]) {
+						pq.add(new Node(next.v, next.w));
+					}
 				}
 			}
-			
+
 			sb.append("#").append(t).append(" ").append(total).append("\n");
 		}
 
 		System.out.println(sb); // 정답 출력
 		br.close();
-	}
-
-	private static boolean union(int x, int y) {
-		x = find(x);
-		y = find(y);
-		if (x == y)
-			return false;
-		else {
-			parents[y] = x;
-			return true;
-		}
-	}
-
-	private static int find(int x) {
-		if (parents[x] == x)
-			return x;
-		return parents[x] = find(parents[x]);
 	}
 }
