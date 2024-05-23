de4295bff86a04c8cfaebb5d97ed615cf7777867
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2024-05-23 22:53:18 +0900

    [Gold V] Title: 포스택, Time: 356 ms, Memory: 30028 KB -BaekjoonHub

--- 백준/Gold/25556. 포스택/포스택.java
+++ 백준/Gold/25556. 포스택/포스택.java
@@ -8,9 +8,6 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Stack<Integer>[] stacks = new Stack[4];
-        Stack<Integer> ans = new Stack<>();
-
-        boolean canClean = true;
 
         for (int i = 0; i < 4; i++) {
             stacks[i] = new Stack<>();
@@ -21,58 +18,23 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             int now = Integer.parseInt(in[i]);
-            int maxTop = -1;
-            int maxIdx = -1;
+            boolean canPush = false;
 
-            // 최대 top 값을 가진 스택을 찾기
             for (int j = 0; j < 4; j++) {
                 if (stacks[j].isEmpty() || stacks[j].peek() < now) {
-                    if (stacks[j].isEmpty() || stacks[j].peek() > maxTop) {
-                        maxTop = stacks[j].isEmpty() ? Integer.MIN_VALUE : stacks[j].peek();
-                        maxIdx = j;
-                    }
-                }
-            }
-
-            if (maxIdx != -1) {
-                stacks[maxIdx].push(now);
-            } else {
-                // 4개의 스택 모두에 넣을 수 없는 경우
-                canClean = false;
-                break;
-            }
-        }
-
-        if (canClean) {
-            while (n > 0) {
-                int maxTop = -1;
-                int maxIdx = -1;
-
-                // 최대 top 값을 가진 스택을 찾기
-                for (int i = 0; i < 4; i++) {
-                    if (!stacks[i].isEmpty() && stacks[i].peek() > maxTop) {
-                        maxTop = stacks[i].peek();
-                        maxIdx = i;
-                    }
-                }
-
-                if (maxIdx != -1) {
-                    int pop = stacks[maxIdx].pop();
-                    if (ans.isEmpty() || ans.peek() >= pop) {
-                        ans.push(pop);
-                    } else {
-                        canClean = false;
-                        break;
-                    }
-                } else {
-                    canClean = false;
+                    stacks[j].push(now);
+                    canPush = true;
                     break;
                 }
-                n--;
+            }
+
+            if (!canPush) {
+                System.out.println("NO");
+                return;
             }
         }
 
-        System.out.println(canClean ? "YES" : "NO");
+        System.out.println("YES");
         br.close();
     }
 }
