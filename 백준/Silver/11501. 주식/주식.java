a7bb16c11f2e1805c3bc88bafe6b20a399b041b9
Wooseobee <87111673+Wooseobee@users.noreply.github.com>
2024-06-17 23:17:15 +0900

    [Silver II] Title: 주식, Time: 1028 ms, Memory: 351988 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -2,29 +2,27 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
+        int t = Integer.parseInt(br.readLine());
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            long benefit = 0;
-            String[] input = br.readLine().split(" ");
-
-            int max = Integer.parseInt(input[n - 1]);
+            String[] in = br.readLine().split(" ");
+            int max = Integer.parseInt(in[n - 1]);
+            long earn = 0;
             for (int j = n - 2; j >= 0; j--) {
-                int v = Integer.parseInt(input[j]);
-                if (v > max) {
+                int v = Integer.parseInt(in[j]);
+                if (v < max) {
+                    earn += max - v;
+                } else {
                     max = v;
-                } else if (v < max) {
-                    benefit += max - v;
                 }
             }
-            sb.append(benefit).append("\n");
+            sb.append(earn).append("\n");
         }
-
         System.out.println(sb);
         br.close();
     }
